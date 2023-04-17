e397e95b01a5c2d74f8845fd4597b67de39b4ede
PeanutBrrutter <117579078+PeanutBrrutter@users.noreply.github.com>
2023-04-17 11:12:24 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -10,6 +10,6 @@ public class Question2
     System.out.print("Enter weight (kg): ");
     double weight = in.nextDouble();
     double bmi = weight/(height*height);
-    System.out.println("Your bmi is: " + bmi);
+    System.out.println(bmi);
   }
 }
