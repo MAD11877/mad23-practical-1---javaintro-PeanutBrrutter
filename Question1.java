34d263e8e3b4340e618224e937432c5912e61819
PeanutBrrutter <117579078+PeanutBrrutter@users.noreply.github.com>
2023-04-17 10:48:50 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,6 +1,7 @@
 public class Question1
 {
-  public static void main(String[] args) { 
-    System.out.println("Hello World!"); 
-  } 
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
 }
