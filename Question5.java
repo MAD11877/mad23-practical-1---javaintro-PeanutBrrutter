88fde41b295c2615ebc5b7cc726edafb8d631681
PeanutBrrutter <117579078+PeanutBrrutter@users.noreply.github.com>
2023-04-18 13:17:18 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -4,19 +4,16 @@ public class Question5
 {
   public static void main(String[] args)
   {
-    Scanner myCount = new Scanner(System.in);
-    int count = myCount.nextInt();
-    myCount.close();
+    Scanner myInput = new Scanner(System.in);
+    int count = myInput.nextInt();
     int[] intArray = new int[count];
     int index = 0;
     while (index<count) {
-      Scanner myInt = new Scanner(System.in);
-      int integer = myInt.nextInt();
-      myInt.close();
+      int integer = myInput.nextInt();
       intArray[index] = integer;
       index++;
     }
-    
+    myInput.close();
     int maxCount = 0, maxInt = 0;
     for (int mainIndex = 0; mainIndex < count; mainIndex++) {
       int intCount = 0;
